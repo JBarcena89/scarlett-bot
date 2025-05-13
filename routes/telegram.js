346274c3a5f5790661f452a5266c938f98c8f23c
JBarcena89 <jjesusbarcenas@gmail.com>
2025-05-13 07:05:33 -0600

    Update telegram.js

--- routes/telegram.js
+++ routes/telegram.js
@@ -1,1 +1,9 @@
-// routes/telegram.js
+const express = require("express");
+const router = express.Router();
+
+// Puedes integrar aquí lógica con node-telegram-bot-api u otra
+router.post("/", (req, res) => {
+  res.send("Telegram endpoint activo");
+});
+
+module.exports = router;
