49b48a7e246ff1f23c177ef7b7fb1eb849f1e8ce
JBarcena89 <jjesusbarcenas@gmail.com>
2025-05-12 23:32:46 -0600

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,1 +1,10 @@
-// models/User.js
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  userId: { type: String, required: true, unique: true },
+  email: String,
+  name: String,
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("User", userSchema);
