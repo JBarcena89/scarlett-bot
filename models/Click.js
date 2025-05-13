8c353d1a2fa6959f512cd7ed71df859e672f6340
JBarcena89 <jjesusbarcenas@gmail.com>
2025-05-13 07:04:09 -0600

    Update Click.js

--- models/Click.js
+++ models/Click.js
@@ -1,1 +1,9 @@
-// models/Click.js
+const mongoose = require("mongoose");
+
+const clickSchema = new mongoose.Schema({
+  userId: String,
+  button: String,
+  timestamp: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("Click", clickSchema);
