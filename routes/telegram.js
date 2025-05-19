eadcddee43967f34e2c3228bacceb08c81f42517
JBarcena89 <jjesusbarcenas@gmail.com>
2025-05-19 10:11:48 -0600

    Update telegram.js

--- routes/telegram.js
+++ routes/telegram.js
@@ -1,9 +1,13 @@
 // routes/telegram.js
 import express from "express";
+import bot from "../services/telegramBot.js";
+
 const router = express.Router();
 
-router.post("/", (req, res) => {
-  res.send("🟢 Endpoint Telegram activo (solo para test)");
+// Telegram enviará mensajes a esta ruta
+router.post("/webhook", (req, res) => {
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
 });
 
 export default router;
