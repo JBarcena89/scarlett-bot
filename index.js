3e9ec11790876a02af7245e4b169d064643aaba0
Jesus <jjesusbarcenas@gmail.com>
2025-05-23 12:58:21 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public'));
+
+require('./telegram')(app);
+require('./webchat')(app);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Scarlett está en línea 💖 en el puerto ${PORT}`);
+});
 
